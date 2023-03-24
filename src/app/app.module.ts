710e752373549b7e7bec77be879123fec9faab4c
Nilupul19 <nilupulmanodya1996@gmail.com>
2023-03-24 07:31:50 +0530

    app module and employee component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { CurrencyformatterPipe } from './pipes/currencyformatter.pipe';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
-    CurrencyformatterPipe
+    CurrencyformatterPipe,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
